51bf6b818612ba3af421a71d9c4871790f230478
Talal <talalstudent@gmail.com>
2021-10-24 19:30:14 +0500

    Cleanup

--- src/containers/Pages/home.tsx
+++ src/containers/Pages/home.tsx
@@ -1,12 +1,9 @@
+import { Box } from "@mui/system";
 import { FunctionComponent } from "react";
-import PrimaryButton from "../../components/Buttons/PrimaryButton";
-import SecondaryButton from "../../components/Buttons/SecondaryButton";
-import OutlinedButton from "../../components/Buttons/OutlinedButton";
-import Input from "../../components/Input/Input";
 
 const Home: FunctionComponent = () => {
   return (
-    <div>
+    <Box>
       <h1>Home</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
@@ -14,12 +11,7 @@ const Home: FunctionComponent = () => {
         et earum adipisci. Labore corrupti dicta excepturi laudantium
         perferendis.
       </p>
-      <PrimaryButton>Hello</PrimaryButton>
-      <SecondaryButton>Hello</SecondaryButton>
-      <OutlinedButton>Hello</OutlinedButton>
-      <Input type="password" label="Password" color="primary"></Input>
-      <Input type="email" label="Email" color="primary"></Input>
-    </div>
+    </Box>
   );
 };
 
--- src/containers/Pages/signup.tsx
+++ src/containers/Pages/signup.tsx
@@ -1,11 +1,12 @@
+import { Box } from "@mui/system";
 import { FunctionComponent } from "react";
 import SignUpForm from "../../components/Forms/SignUpForm";
 
 const Signup: FunctionComponent = () => {
   return (
-    <>
+    <Box>
       <SignUpForm />
-    </>
+    </Box>
   );
 };
 
