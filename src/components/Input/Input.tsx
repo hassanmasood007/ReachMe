c778b4e461cc53b8dcb5ac58c666f8d70f9620c1
Muhammad Talal <62650683+itxtalal@users.noreply.github.com>
2021-11-16 19:43:47 +0500

    Update Input.tsx

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-labels */
 import { TextField, withStyles } from "@material-ui/core";
-
+// eslint-disable-next-line
 const Input = withStyles((theme) => ({
   root: {
     background: "inherit",
